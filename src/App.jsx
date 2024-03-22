c62ea41d8180c67fb4c213507680a89bb3975554
Emily Gao Wang <egwang1@uci.edu>
2024-03-22 15:54:23 -0700

    First push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
